c7ad48706becba004858cd1723431a2f6dccf6fe
mareeyamnarak <s65309010029@ptnicec.ac.th>
2022-10-20 18:18:27 +0700

    ส่ง test.php ครั่งที่1

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "mareyam leedeejai";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+</head>
+<body>
+   <br> INFORMATION TECHNOLOGE <br>
+   <?php
+    echo "pattani";
+   ?>
+</body>
+</html>
+<?php
+  echo "<br> thaland"
+  ?>
